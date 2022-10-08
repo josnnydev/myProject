c4d7262bcd6257cace750ead7f8d80a06514c772
josnnydev <pjosnny@gmail.com>
2022-10-07 22:24:42 -0400

    add verbs

--- src/models/registroUser.js
+++ src/models/registroUser.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose')
 
-const newSchema = mongoose.Schema({
+const NewSchema = new mongoose.Schema({
     name: {type: String, required: true},
-    //lastName: {type: String, required: true}
-    // age: {type: Number, required: true},
-    // password: {type: String, required: true},
-    // date_at: {type: Date, default: Date.now}
+    lastName: {type: String, required: true},
+      age: {type: Number, required: true},
+     password: {type: String, required: true},
+    date_at: {type: Date, default: Date.now}
 
 },{versionKey: false})
 
-module.exports = mongoose.model('registro', newSchema)
+module.exports = mongoose.model('registro', NewSchema)
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,20 +1,87 @@
 const express = require('express')
 const router = express.Router()
-const newSchema = require('../models/registroUser')
+const NewSchema = require('../models/registroUser')
 
 
 router.post('/handleForm',(req,res)=>{
      
      
-    const newData=new newSchema({
-      name: req.body.name
+    let newData=new NewSchema({
+      name: req.body.name,
+      lastName: req.body.lastName,
+      age: req.body.age,
+      password: req.body.password
     })
-
+    
     newData.save()
-    .then((data => console.log(newData)))
-    .catch((e)=> console.log(e))
-    res.send('formulario enviado')
+    .then((data => res.send(`Te haz registrado correctamente... ${data.name}`)))
+     .catch((e)=> console.log(e))
+     
  })
+  
+ // search all users
+ router.get('/user/all',(req,res)=>{
+   NewSchema.find((error,data)=>{
+  if(error){
+    console.log(error)
+  }else{
+    res.json(data)
+  }
+  })
+   
+ })
+
+ //search last user
+ router.get('/user/last',(req,res)=>{
+  NewSchema.find().skip(1).exec((error,data)=>{
+ if(error){
+   console.log(error)
+ }else{
+   res.json(data)
+ }
+ })
+  
+})
+
+ //search first user
+ router.get('/user', (req,res)=>{
+  
+   NewSchema.findOne((error,data)=>{
+  if(error){
+    console.log(error)
+  }else{
+    res.json(data)
+  }
+  })
+   
+ })
+
+ //search one user for id
+ router.get('/user/id', (req,res)=>{
+  
+  NewSchema.findById('633f7ed3de77da65e5f924d6',(error,data)=>{
+  if(error){
+    res.status(404).send({message: error})
+  }else{
+    res.json(data)
+  }
+  })
+   
+ })
+ 
+ //delete user
+ router.get('/user/delete', (req,res)=>{
+  
+  NewSchema.findByIdAndRemove('633f7ed3de77da65e5f924d6',(error,data)=>{
+  if(error){
+    res.status(404).send(error)
+  }else{
+    res.send('usuario eliminado')
+  }
+  })
+   
+ })
+
 
 
 module.exports = router
